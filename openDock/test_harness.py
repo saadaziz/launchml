967d057129600e4597473938065b57b3171de82e
Saad Aziz <saad0914@gmail.com>
2023-07-13 22:24:40 -0700

    Specification

---
+++ openDock/test_harness.py
@@ -0,0 +1,11 @@
+# Train test Algorithm test harness
+# Description:
+# - The train_test is a method that is used to evaluate a machine learning algorithm
+# Assumptions:
+# - Function to split dataset into training and testing datasets
+# - Function to evaluate the accuracy of the algorithm's predictions
+# Objectives:
+# - Consume dataset, and an algorithm
+# - Evaluate the accuracy of the algorithm's predictions by returning a performance score
+
+# def train_test(self, dataset, algorithm, split, *args):
